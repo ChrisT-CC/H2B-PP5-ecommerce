6489d7832ff444241f363fa5a8c9261b07e69f82
ChrisT-CC <cristaecc@gmail.com>
2022-07-12 20:57:04 +0000

    Add products app and basic files for views and urls

---
+++ products/views.py
@@ -0,0 +1,8 @@
+""" Products app views file """
+from django.shortcuts import render
+
+
+def all_products(request):
+    """ A view to show all products """
+
+    return render(request, 'products/products.html')
