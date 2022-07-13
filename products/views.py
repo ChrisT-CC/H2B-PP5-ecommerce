fb561c867288b57ce2a7141e91f709dfbc739881
ChrisT-CC <cristaecc@gmail.com>
2022-07-13 12:30:44 +0000

    Update products template and view to render all products. Fix favicon to show in products page

--- products/views.py
+++ products/views.py
@@ -1,8 +1,15 @@
 """ Products app views file """
 from django.shortcuts import render
+from .models import Product
 
 
 def all_products(request):
     """ A view to show all products """
 
-    return render(request, 'products/products.html')
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
