4e5d03bb6b57abab03e4f59660b0d1b8fe21f2ff
ChrisT-CC <cristaecc@gmail.com>
2022-08-29 08:15:06 +0000

    Add sorting functionality

--- products/views.py
+++ products/views.py
@@ -2,17 +2,47 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
     """ A view to show all products """
-
+    # Get all products and categories from DB
     products = Product.objects.all()
+    all_categories = Category.objects.all()
 
+    # Initialize variables
     query = None
+    categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        # Sorting functionality
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        # Sort by categories
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        # Sort by product availability
+        if 'stock' in request.GET:
+            products = products.filter(quantity__gt=0)
+
+        # Search functionality
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,9 +54,14 @@ def all_products(request):
                        Q(description__icontains=query))
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
+        'all_categories': all_categories,
         'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
